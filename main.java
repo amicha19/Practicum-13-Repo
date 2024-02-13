54dd88fd6a794ad49f0681b2c0e74c9f31cceab7
amicha19 <158494961+amicha19@users.noreply.github.com>
2024-02-13 06:08:20 -0500

    Update main.java

--- main.java
+++ main.java
@@ -4,28 +4,36 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class practicumnum13 {
 
     public static void main(String[] args) {
 
         // var declarations
-        int n, sum1, sum2;
+        int n;
+        int sum1, 
+        int sum2;
+        int count = 0;
      
-        Scanner input = new Scanner(System.in);
+        Scanner user_input = new Scanner(System.in);
 
         // prompt user for value of n to sum up to
         // if then enter 5, should add up 1 to 5 (equal to 15)
-       
+       System.out.println("Enter a the max value: ");
+        n = user_input.nextInt();
 
         // use a for loop to sum up the values and assign to sum1
-
+                for (int i = 1; i<= n; i++) {
+                    sum1 = sum1 + i;
+                }
 
         // use a while loop to sum up the values and assign to sum2
-        
+                while( count <= n) {
+                    sum2 = sum2 + count;
+                    count++;
+                }
 
         // display results
-        System.out.println(
-          "Sum of 1.." + n + " by use of for loop is " + sum1);
+        System.out.println("Sum of 1.." + n + " by use of for loop is " + sum1);
 
         System.out.println(
           "Sum of 1.." + n + " by use of while loop is " + sum2);
